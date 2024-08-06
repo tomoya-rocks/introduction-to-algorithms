98328d5c96a59d9387a88fa7b921982b706b1ef1
Tomoya Sasaki <t.sasaki@future-shop.co.jp>
2024-08-06 10:07:51 +0900

    Implement fractional knapsack problem

--- greedy_algorithms/fractional_knapsack_problem.py
+++ greedy_algorithms/fractional_knapsack_problem.py
@@ -1,3 +1,24 @@
+def select_items(weights, prices, limit):
+    unit_prices = {}
+    for i, (price, weight) in enumerate(zip(prices,  weights)):
+        unit_prices[i] = price / weight
+
+    sorted_unit_prices = sorted(
+        unit_prices.items(), key=lambda x: x[1], reverse=True)
+
+    rest = limit
+    results = {}
+    for idx, _ in sorted_unit_prices:
+        weight_to_load = weights[idx] if weights[idx] <= rest else rest
+        results[idx] = weight_to_load
+
+        rest -= weight_to_load
+        if rest == 0:
+            break
+
+    return results
+
+
 if __name__ == '__main__':
     weights = [20, 30, 10]
     prices = [100, 120, 60]
@@ -14,3 +35,12 @@ if __name__ == '__main__':
     for _p in prices:
         print(f"{_p:>3}|", end='')
     print('')
+
+    item_to_select = select_items(weights, prices, 50)
+    print("--- result ---")
+    total = 0.0
+    for idx, weight in item_to_select.items():
+        print(f"item = {idx}, weight = {weight}")
+
+        total += weight * (prices[idx] / weights[idx])
+    print(f"total = {total}")
