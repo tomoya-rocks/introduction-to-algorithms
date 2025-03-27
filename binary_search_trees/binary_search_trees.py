8d72ed7b18b1884f614188b17de5f335e10cacda
Tomoya Sasaki <tomoya.let.it.bleed@gmail.com>
2025-03-28 08:53:21 +0900

    Implement binary search tree

--- binary_search_trees/binary_search_trees.py
+++ binary_search_trees/binary_search_trees.py
@@ -56,6 +56,28 @@ class Tree:
 
             return q
 
+    def remove(self, z):
+        self.size -= 1
+
+        if not z.left:
+            self.__transplant(z, z.right)
+        elif not z.right:
+            self.__transplant(z, z.left)
+        else:
+            y = self.successor(z)
+            if y != z.right:
+                self.__transplant(y, y.right)
+                y.right = z.right
+                y.right.parent = y
+
+            self.__transplant(z, y)
+            y.left = z.left
+            y.left.parent = y
+
+        z = None
+
+        self.height = self.__get_height()
+
     def print_tree(self):
 
         def print_tree_internal(r, space_size=0):
@@ -71,6 +93,17 @@ class Tree:
 
         if self.root:
             print_tree_internal(self.root)
+
+    def __transplant(self, p, q):
+        if not p.parent:
+            self.root = q
+        elif p == p.parent.left:
+            p.parent.left = q
+        else:
+            p.parent.right = q
+
+        if q:
+            q.parent = p.parent
 
     def __get_height(self):
 
@@ -95,16 +128,6 @@ class Tree:
 if __name__ == '__main__':
     t = Tree()
 
-    t.put(Element(8))
-    t.put(Element(3))
-    t.put(Element(10))
-    t.put(Element(1))
-    t.put(Element(6))
-    t.put(Element(14))
-    t.put(Element(4))
-    t.put(Element(7))
-    t.put(Element(13))
-
     while True:
         print("1:put 2:remove 3:print 4:successor > ", end='')
         op = int(input())
@@ -115,7 +138,14 @@ if __name__ == '__main__':
 
             t.put(Element(key))
         elif op == 2:
-            pass
+            print("input key > ", end='')
+            key = int(input())
+
+            z = t.contains(key)
+            if not z:
+                print(f"{key} is not found in the tree.")
+            else:
+                t.remove(z)
         elif op == 3:
             t.print_tree()
         elif op == 4:
@@ -128,7 +158,7 @@ if __name__ == '__main__':
             else:
                 succ = t.successor(z)
                 if not succ:
-                    print(f"successor is none.")
+                    print("successor is none.")
                 else:
                     print(f"successor = {succ.key}")
         else:
