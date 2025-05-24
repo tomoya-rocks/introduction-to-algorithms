2c7aa47d10c5fb78f786c1f98efdae1b63cd75c7
Tomoya Sasaki <tomoya@TomoyanoMacBook-Air.local>
2025-05-24 10:30:27 +0900

    Implement method to remove key

--- b_trees/b_trees.py
+++ b_trees/b_trees.py
@@ -100,7 +100,7 @@ class Tree:
                         if not y.is_leaf:
                             z.children.insert(0, y.children[-1])
                             y.children = y.children[:-1]
-                    elif i <= r.n and r.children[i + 1].n >= self.min_degree:
+                    elif i <= r.n - 1 and r.children[i + 1].n >= self.min_degree:
                         y = r.children[i]
                         z = r.children[i + 1]
 
@@ -115,6 +115,47 @@ class Tree:
                         if not z.is_leaf:
                             y.children.append(z.children[0])
                             z.children = z.children[1:]
+                    else:
+                        if i - 1 >= 0 and r.children[i - 1].n == self.min_degree - 1:
+                            y = r.children[i - 1]
+                            z = r.children[i]
+                            key_to_down = r.keys[i - 1]
+
+                            y.keys.append(key_to_down)
+                            y.n += 1
+                            y.keys.extend(z.keys)
+                            y.n += z.n
+
+                            if not y.is_leaf:
+                                y.children.extend(z.children)
+
+                            r.keys.remove(key_to_down)
+                            r.n -= 1
+                            r.children.pop(i)
+
+                            if len(r.keys) == 0:
+                                self.root = y
+
+                            i -= 1
+                        elif i <= r.n - 1 and r.children[i + 1].n == self.min_degree - 1:
+                            y = r.children[i]
+                            z = r.children[i + 1]
+                            key_to_down = r.keys[i]
+
+                            y.keys.append(key_to_down)
+                            y.n + 1
+                            y.keys.extend(z.keys)
+                            y.n += z.n
+
+                            if not y.is_leaf:
+                                y.children.extend(z.children)
+
+                            r.keys.remove(key_to_down)
+                            r.n -= 1
+                            r.children.pop(i + 1)
+
+                            if len(r.keys) == 0:
+                                self.root = y
 
                 remove_internal(r.children[i], key)
 
@@ -180,12 +221,12 @@ if __name__ == '__main__':
     t.insert(9)
     t.insert(3)
     t.insert(7)
-    #t.insert(1)
-    #t.insert(2)
-    #t.insert(8)
-    #t.insert(6)
-    #t.insert(0)
-    #t.insert(4)
+    t.insert(1)
+    t.insert(2)
+    t.insert(8)
+    t.insert(6)
+    t.insert(0)
+    t.insert(4)
 
     while True:
         print("1:insert 2:remove 3:print > ", end='')
