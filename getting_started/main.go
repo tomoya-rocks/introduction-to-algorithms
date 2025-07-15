896cb8ff9f1db7d277135a39a934026647ce0f7a
Tomoya Sasaki <tomoya.let.it.bleed@gmail.com>
2025-07-15 10:30:45 +0900

    Implement selection sort

--- getting_started/main.go
+++ getting_started/main.go
@@ -8,8 +8,20 @@ import (
 func main() {
 	data := sort_algorithm.CreateData()
 
+	fmt.Println("--- before ---")
 	fmt.Println(data)
 
-	fmt.Println("--- insertion sort ---")
-	sort_algorithm.InsertionSort(data)
+	var op string
+	fmt.Print("input sort (1:insertion sort 2:selection sort) > ")
+	fmt.Scanf("%s", &op)
+
+	switch op {
+	case "1":
+		sort_algorithm.InsertionSort(data)
+	case "2":
+		sort_algorithm.SelectionSort(data)
+	}
+
+	fmt.Println("--- after ---")
+	fmt.Println(data)
 }
